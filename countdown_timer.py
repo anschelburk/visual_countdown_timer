4540fea1e5d56a0d3199ed5fbd5bca6a04fcaf04
Anschel Burk <anschel.burk@gmail.com>
2025-07-03 12:32:46 -0500

    Add docstring to clean_text() function.

--- countdown_timer.py
+++ countdown_timer.py
@@ -4,6 +4,15 @@ import os
 import time
 
 def clean_text(unformatted_text):
+    """
+    Removes leading and trailing spaces and common punctuation from a string.
+
+    Args:
+        unformatted_text (str): The string to be cleaned.
+
+    Returns:
+        str: The cleaned string with specified characters removed from both ends.
+    """
     clean_text = unformatted_text.strip(" .,\"\'")
     return clean_text
 
