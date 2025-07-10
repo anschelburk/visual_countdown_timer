673ceebba8b4835d1c3aab93cfb5b6c7a4a22deb
Anschel Burk <anschel.burk@gmail.com>
2025-07-10 13:29:43 -0400

    Refactor progress_bar() function from timer/actions.py to timer/variables.py

--- src/visual_countdown_timer/timer/actions.py
+++ src/visual_countdown_timer/timer/actions.py
@@ -5,17 +5,14 @@ from .constants import (
     )
 from .variables import (
     current_date,
-    current_time
+    current_time,
+    progress_bar
     )
 from .support import (
     clean_text,
     clear_terminal
     )
-from datetime import (
-    datetime,
-    timedelta
-    )
-import math
+from datetime import datetime, timedelta
 import time
 
 def confirm_user_input(input_to_confirm):
@@ -66,41 +63,6 @@ def print_title_block():
     print('Visual Countdown Timer')
     print('Press Ctrl + C to exit.')
     print(f'{THICK_HORIZONTAL_LINE}\n')    
-
-def progress_bar(remaining_time_in_seconds):
-    """
-    Generates a visual progress bar representing the remaining time until the next hour.
-
-    Args:
-        remaining_time_in_seconds (int): An integer which represents the total remaining time in seconds
-            until the current timer finishes counting down.
-
-    Returns:
-        progress_bar_text (str):
-            A visual progress bar composed of '#' and '.' characters showing the remaining
-            amount of time until the current timer finishes counting down.
-
-            Each '#' represents about 2 minutes of remaining time. (Every 2 minutes, the '#' character furthest
-            to the right is replaced by a '.' character.) The '.' characters represent elapsed time. The
-            number of minutes is rounded up if any seconds are left beyond a full minute to ensure a more
-            intuitive countdown.
-
-            The bar is 32 characters wide: 30 total '#' and '.' characters, enclosed in a left and right bracket.
-
-            Examples:
-
-            - 16 minutes, 00 seconds → 16 minutes → 8 filled ('#') characters and 22 empty ('.') characters, shown below.
-                [########......................]
-            
-            - 16 minutes, 01 second  → 17 minutes → 9 filled ('#') characters and 21 empty ('.') characters, shown below.
-                [#########.....................]
-    """
-
-    remaining_minutes_rounded = math.ceil(remaining_time_in_seconds / 60)
-    progress_bar_full = '#' * round(remaining_minutes_rounded / 2)
-    progress_bar_empty = '.' * (30 - round(remaining_minutes_rounded / 2))
-    progress_bar_text = f'[{progress_bar_full}{progress_bar_empty}]'
-    return progress_bar_text
 
 def run_timer():
         """
--- src/visual_countdown_timer/timer/variables.py
+++ src/visual_countdown_timer/timer/variables.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import math
 
 def current_date():
     """
@@ -21,3 +22,38 @@ def current_time():
     """
     current_time = datetime.now().astimezone().strftime('%H:%M %Z')
     return current_time
+
+def progress_bar(remaining_time_in_seconds):
+    """
+    Generates a visual progress bar representing the remaining time until the next hour.
+
+    Args:
+        remaining_time_in_seconds (int): An integer which represents the total remaining time in seconds
+            until the current timer finishes counting down.
+
+    Returns:
+        progress_bar_text (str):
+            A visual progress bar composed of '#' and '.' characters showing the remaining
+            amount of time until the current timer finishes counting down.
+
+            Each '#' represents about 2 minutes of remaining time. (Every 2 minutes, the '#' character furthest
+            to the right is replaced by a '.' character.) The '.' characters represent elapsed time. The
+            number of minutes is rounded up if any seconds are left beyond a full minute to ensure a more
+            intuitive countdown.
+
+            The bar is 32 characters wide: 30 total '#' and '.' characters, enclosed in a left and right bracket.
+
+            Examples:
+
+            - 16 minutes, 00 seconds → 16 minutes → 8 filled ('#') characters and 22 empty ('.') characters, shown below.
+                [########......................]
+            
+            - 16 minutes, 01 second  → 17 minutes → 9 filled ('#') characters and 21 empty ('.') characters, shown below.
+                [#########.....................]
+    """
+
+    remaining_minutes_rounded = math.ceil(remaining_time_in_seconds / 60)
+    progress_bar_full = '#' * round(remaining_minutes_rounded / 2)
+    progress_bar_empty = '.' * (30 - round(remaining_minutes_rounded / 2))
+    progress_bar_text = f'[{progress_bar_full}{progress_bar_empty}]'
+    return progress_bar_text
