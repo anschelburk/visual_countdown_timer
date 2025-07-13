d00b4da0befe1701896c3807a5e15f7cbd1473f3
Anschel Burk <anschel.burk@gmail.com>
2025-07-13 13:29:05 -0500

    Fix issue with how text is displayed.

--- src/visual_countdown_timer/timer/actions.py
+++ src/visual_countdown_timer/timer/actions.py
@@ -18,8 +18,8 @@ import time
 
 def confirm_hour_format():
     print("Would you like the time to display as 12 hours or 24 hours?")
-    print("{INDENT}12 hours looks like this: 3:52pm")
-    print("{INDENT}24 hours looks like this: 15:52")
+    print(f"{INDENT}12 hours looks like this: 3:52pm")
+    print(f"{INDENT}24 hours looks like this: 15:52")
     user_input = input("Type \"12\" to format as 12 hours, or \"24\" to format as 24 hours: ")
     user_hours = int(clean_text(user_input))
     return user_hours
