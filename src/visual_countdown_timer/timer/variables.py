dad99d94d89e3e096c709fe02ab0694f504494ad
Anschel Burk <anschel.burk@gmail.com>
2025-07-15 14:43:04 -0400

    Remove old current_date() function.

--- src/visual_countdown_timer/timer/variables.py
+++ src/visual_countdown_timer/timer/variables.py
@@ -3,17 +3,6 @@ import math
 
 current_date = datetime.now().strftime('%B %d, %Y')
 
-# def current_date():
-#     """
-#     Calculates the current date, formatted as follows: [Month] [Day], [Year].
-#     Args:
-#         None.
-#     Returns:
-#         current_date (datetime): a datetime object, formatted as described above.
-#     """
-#     current_date = datetime.now().strftime('%B %d, %Y')
-#     return current_date
-
 def next_occurrence(current_datetime, target_minute):
     """
     Returns the next datetime where the minute equals target_minute.
