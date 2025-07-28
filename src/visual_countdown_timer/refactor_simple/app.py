2a1621c5f0f944026acc3290c7f892d4208a5b35
Anschel Burk <anschel.burk@gmail.com>
2025-07-28 12:26:10 -0400

    Rename refactoring directories for added clarity. Put main files in simple refactoring directory.

---
+++ src/visual_countdown_timer/refactor_simple/app.py
@@ -0,0 +1,5 @@
+from .timer.timer_app import TimerApp
+
+def main():
+	timer_app = TimerApp()
+	timer_app.run()
---
+++ src/visual_countdown_timer/refactor_simple/timer/timer_app.py
@@ -0,0 +1,11 @@
+class TimerApp(self):
+
+	"""
+	Main timer application class that coordinates all components.
+	"""
+
+	def __init__(self):
+		pass
+
+	def run(self):
+		pass
