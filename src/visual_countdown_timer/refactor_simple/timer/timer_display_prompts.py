fd6b84ca3f1b186cda36ffd8dbd21075ff3f4ced
Anschel Burk <anschel.burk@gmail.com>
2025-07-29 22:13:22 -0400

    Divide TextBlocks class into two classes: _TextElements (smaller elements) and PrintTextBlock (multi-line text blocks).

--- src/visual_countdown_timer/refactor_simple/timer/timer_display_prompts.py
+++ src/visual_countdown_timer/refactor_simple/timer/timer_display_prompts.py
@@ -4,14 +4,10 @@ from timer_utils import TerminalUtils
 # Add docstring for module.
 """
 
-class TextBlocks:
-    """
-    Pre-built text blocks derived from configuration settings.
+class TextElements:
 
-    Contains display strings and formatting blocks that are calculated
-    from base configuration values. These are ready-to-use
-    blocks of text for building the timer interface, including indentation
-    strings and horizontal divider lines.
+    """
+    Simple text elements derived from configuration settings.
 
     All blocks in this class are derived from the base measurements
     defined in DisplaySettings, providing consistent formatting throughout
@@ -22,16 +18,26 @@ class TextBlocks:
     THICK_HORIZONTAL_LINE = DisplaySettings.LINE_LENGTH * '='
     THIN_HORIZONTAL_LINE = DisplaySettings.LINE_LENGTH * '-'
 
-    @classmethod
+class PrintTextBlock:
+    """
+    Pre-built text blocks built from simpler elements in _TextElements class.
+
+    Contains display strings and formatting blocks that are calculated
+    from base configuration values. These are ready-to-use
+    blocks of text for building the timer interface, including indentation
+    strings and horizontal divider lines.
+    """
+
+    @staticmethod
     def timer_title_text():
-        print(cls.THICK_HORIZONTAL_LINE)
+        print(TextElements.THICK_HORIZONTAL_LINE)
         print('Visual Countdown Timer')
         print('Press Ctrl + C to exit.')
-        print(cls.THICK_HORIZONTAL_LINE)
+        print(TextElements.THICK_HORIZONTAL_LINE)
 
-    @classmethod
-    def timer_introduction_text():
-        print('Welcome to Visual Countdown Timer!')
+    @staticmethod
+    def timer_app_introduction():
+        print('\nWelcome to Visual Countdown Timer!')
         print('This timer counts down to a set number of minutes past each hour.')
         print('For example, if you enter \"25\", it will count down to 1:25, 2:25, etc.\n')
 
